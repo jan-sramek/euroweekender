0fb4bbd742559dc64f0e0db28c1c4200b4bef08d
Jan Šrámek <jsramek@mutualvision.co.uk>
2026-07-07 23:11:50 +0200

    Reduce default flight crawl interval to 10 minutes.
    Update CrawlOptions, worker appsettings, and docker-compose defaults from 20 to 10.

--- WeekendFlights.Application/Models/CrawlOptions.cs
+++ WeekendFlights.Application/Models/CrawlOptions.cs
@@ -7,5 +7,5 @@ public sealed class CrawlOptions
     public int UpcomingWeeks { get; init; } = 4;
     public int MaxCitiesPerRun { get; init; } = 25;
     public int RequestDelayMs { get; init; } = 2000;
-    public int IntervalMinutes { get; init; } = 20;
+    public int IntervalMinutes { get; init; } = 10;
 }
